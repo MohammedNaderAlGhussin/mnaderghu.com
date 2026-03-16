aa705a3b38e1cacfa8cff41eeb8112076a6ccee0
Mohammed Nader Al Ghussin <business.naderghu@gmail.com>
2026-03-16 09:19:30 +0200

    removing unused imports

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import { useTranslation } from "react-i18next";
-import { Github, Twitter, Linkedin, Mail } from "lucide-react";
+import { Github, Linkedin, Mail } from "lucide-react";
 import Link from "next/link";
 
 export function Footer() {
